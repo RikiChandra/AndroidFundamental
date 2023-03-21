7580f829fcbc36e52847aa37249dec4eabec83c5
RikiChandra <49185211+RikiChandra@users.noreply.github.com>
2023-03-21 07:58:35 +0700

    fix bugs splashscreen

--- app/src/main/java/com/example/githubapp/SplashActivity.kt
+++ app/src/main/java/com/example/githubapp/SplashActivity.kt
@@ -4,27 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.example.githubapp.view.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
 
-        supportActionBar?.hide()
-
-        window.setFlags(
-            android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN, android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-        val durasion = 3000L
-
-        Thread {
-            Thread.sleep(durasion)
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
             finish()
-        }.start()
+        }, splashTimeOut)
+
+
     }
 
 
